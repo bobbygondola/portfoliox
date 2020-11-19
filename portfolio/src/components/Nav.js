15cda3b304e181ecdba760d4769ac145f7e06758
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-11-19 13:09:06 -0500

    SETUP-DONE/react up, needs stlyes and function

---
+++ portfolio/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../images/logoo.png'
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <div id="ooo"><img id="logo" alt="logo" src={logo} /></div>
+            <div><h3>Software Engineer</h3></div>
+        </div>
+    )
+}
+
+export default Nav
