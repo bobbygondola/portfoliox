2f6d35eb2f78961b40d44a5df28afaa9ee627069
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-11-19 15:38:33 -0500

    FIX/cleared node_modules, reinstalled

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Mission from './components/Mission'
 import About from './components/About'
-import Contact from './components/Contact'
+import ContactPage from './components/ContactPage'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Projects />
       <Mission />
       <About />
-      <Contact />
+      <ContactPage />
     </div>
   );
 }
