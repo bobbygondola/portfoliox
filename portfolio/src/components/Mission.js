2cd1bda9f9f6ab7148cc3b3518f4c46984beb032
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-11-29 17:17:57 -0500

    STYLES/mission

--- portfolio/src/components/Mission.js
+++ portfolio/src/components/Mission.js
@@ -4,6 +4,12 @@ import "../styles/css/App.css"
 const Mission = () => {
     return (
         <div className="missionComponent" id="2">
+
+            <div className="elonQuote">
+                <h3>"People should pursue what they're passionate about. That will make them happier than pretty much anything else."</h3><span>- Elon Musk</span>
+            </div>
+
+        
             
         </div>
     )
